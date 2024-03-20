3578b3401b8985288f4d03fef665710f54e8a609
Janne Saari <jannesaari24@gmail.com>
2024-03-20 11:38:49 +0200

    Change cli.js to use Blog Model

--- cli.js
+++ cli.js
@@ -1,14 +1,45 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize(encodeURI(process.env.PGHOST), {logging: false})
 
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author:{
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title:{
+    type: DataTypes.TEXT,
+    allowNull:false
+  },
+  likes:{
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+},
+{
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
 const main = async () => {
   try {
+
     await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    const blogs = await Blog.findAll()
     blogs.map(blog => {
-      console.log(`${blog.author}: '${blog.title}', likes: ${blog.likes}`)
+      console.log(`${blog.dataValues.author}: '${blog.dataValues.title}', likes: ${blog.dataValues.likes}`)
     })
 
     sequelize.close()
