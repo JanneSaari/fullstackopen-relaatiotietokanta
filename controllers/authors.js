f816cab315004d2d62d0549b2687e357549296e8
Janne Saari <jannesaari24@gmail.com>
2024-03-25 13:37:49 +0200

    Add endpoint GET /api/authors, finish 13.16

---
+++ controllers/authors.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const { Op } = require('sequelize')
+
+const { Blog } = require('../models')
+const { sequelize } = require('../util/db')
+
+router.get('/', async(req, res) => {
+  const blogs = await Blog.findAll({
+    attributes:[
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('author')), 'blogs'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+    ],
+    group: 'author'
+  })
+
+  res.json(blogs)
+})
+
+module.exports = router
