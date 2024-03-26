7aa0b6051d57e28dcc93f046f8f32e221efdb16d
Janne Saari <jannesaari24@gmail.com>
2024-03-26 13:59:32 +0200

    Add PUT endpoint for editing readingState, finish 13.22

--- controllers/readinglist.js
+++ controllers/readinglist.js
@@ -11,4 +11,17 @@ router.post('/', async(req, res) => {
   return res.json(newListEntry)
 })
 
+router.put('/:id', async (req, res) => {
+  if(!req.body.readingState){
+    throw new Error("readingState not defined in request body")
+  }
+  let listEntry = await ReadingList.findByPk(req.params.id)
+  if(!listEntry){
+    throw new Error(`could not find ReadingList entry with this id: ${req.params.id}`)
+  }
+  listEntry.readingState = req.body.readingState
+  await listEntry.save()
+  res.json(listEntry)
+})
+
 module.exports = router
