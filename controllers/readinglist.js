44454e6caa465afdcb26d8eb3053620a6a03aae3
Janne Saari <jannesaari24@gmail.com>
2024-03-26 14:08:44 +0200

    Change readingState updating to only work with valid user token, fix 13.22

--- controllers/readinglist.js
+++ controllers/readinglist.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const { Op } = require('sequelize')
 
-const { ReadingList } = require('../models')
+const { ReadingList, User } = require('../models')
 const { sequelize } = require('../util/db')
+const tokenExtractor = require('../middleware/tokenExtractor')
+
 
 router.post('/', async(req, res) => {
   console.log('body', req.body)
@@ -11,13 +13,17 @@ router.post('/', async(req, res) => {
   return res.json(newListEntry)
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', tokenExtractor, async (req, res) => {
   if(!req.body.readingState){
     throw new Error("readingState not defined in request body")
   }
   let listEntry = await ReadingList.findByPk(req.params.id)
   if(!listEntry){
     throw new Error(`could not find ReadingList entry with this id: ${req.params.id}`)
+  }
+  const user = await User.findByPk(req.decodedToken.id)
+  if(user.id !== listEntry.userId){
+    throw new Error(`token is not valid for editing this entry, you can only edit your own entries`)
   }
   listEntry.readingState = req.body.readingState
   await listEntry.save()
